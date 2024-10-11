e9b3307863bed47799a14b6d9dc53dbb417bcbe3
Anaprincs <Anaprincs>
2024-10-11 15:42:49 -0300

    first commit

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\TarefaController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::post('tarefa', [TarefaController::class, 'store']);
+Route::get('tarefa/{id}', [TarefaController::class, 'findById']);
+Route::get('tarefa/{id}/Request', [TarefaController::class, 'findByIdRequest']);
