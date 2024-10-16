cd574b2bcacaa2db5e6ce6cf243bf5fb4f15e6d3
Anaprincs <161864577+Anaprincs@users.noreply.github.com>
2024-10-16 15:52:06 -0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -11,3 +11,8 @@ Route::get('/user', function (Request $request) {
 Route::post('tarefa', [TarefaController::class, 'store']);
 Route::get('tarefa/{id}', [TarefaController::class, 'findById']);
 Route::get('tarefa/{id}/Request', [TarefaController::class, 'findByIdRequest']);
+Route::get('tarefa', [TarefaController::class, 'getAll']);
+Route::delete('tarefa/{id}/deletar', [TarefaController::class, 'delete']);
+Route::delete('tarefa/{id}/deletar', [TarefaController::class, 'delete']);
+Route::put('tarefa/{id}/update', [TarefaController::class, 'update']);
+Route::put('tarefa/pesquisa/mes', [TarefaController::class, 'pesquisa']);
